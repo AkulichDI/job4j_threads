498eef86b00ef0e22a772ed2487dc885a682e50b
Gerik228 <dimaakulichd6618@gmail.com>
2026-05-12 11:41:32 +1000

    4. Режим ожидания. [#231217 #263205]

--- src/main/java/concurrent/ThreadState.java
+++ src/main/java/concurrent/ThreadState.java
@@ -2,16 +2,30 @@ package concurrent;
 
 
 
-public class ThreadState {
-    public static void main(String[] args) {
-        Thread first = new Thread(
-                () -> {}
-        );
-        System.out.println(first.getState());
-        first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
+    public class ThreadState {
+        public static void main(String[] args) {
+            Thread first = new Thread(
+                    () -> System.out.println(Thread.currentThread().getName())
+            );
+
+            Thread second = new Thread(
+                    () -> System.out.println(Thread.currentThread().getName())
+            );
+
             System.out.println(first.getState());
+            System.out.println(second.getState());
+
+            first.start();
+            second.start();
+
+            while (first.getState() != Thread.State.TERMINATED
+                    || second.getState() != Thread.State.TERMINATED) {
+                System.out.println(first.getState() + " " + first.getName());
+                System.out.println(second.getState() + " " + second.getName());
+            }
+
+            System.out.println(first.getState());
+            System.out.println(second.getState());
+            System.out.println("Work completed");
         }
-        System.out.println(first.getState());
     }
-}
