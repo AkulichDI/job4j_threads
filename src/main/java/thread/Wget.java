9c44ff9f415ee91ca519e89d6cd406c1d8a64c7b
Gerik228 <dimaakulichd6618@gmail.com>
2026-05-13 15:53:33 +1000

    8. Скачивание файла с ограничением. [#144271 #263703]

--- src/main/java/thread/Wget.java
+++ src/main/java/thread/Wget.java
@@ -1,6 +1,13 @@
 package thread;
 
-public class Wget implements Runnable {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Wget  implements Runnable{
 
     private final String url;
     private final int speed;
@@ -10,8 +17,56 @@ public class Wget implements Runnable {
         this.speed = speed;
     }
 
+
     @Override
     public void run() {
+
+        var startAt = System.currentTimeMillis();
+        var file = new File(url.substring(url.lastIndexOf('/') + 1));
+        String url = this.url;
+
+        try (var input = new URL(url).openStream();
+             var output = new FileOutputStream(file)) {
+
+
+            int bytesRead;
+            int downloaded = 0;
+            long start = System.currentTimeMillis();
+
+
+            System.out.println("Open connection: " + ((double)(System.currentTimeMillis() - startAt)/ 1000) + " seconds.");
+
+
+            var dataBuffer = new byte[this.speed ];
+
+            while ((bytesRead = input.read(dataBuffer)) != -1) {
+                output.write(dataBuffer, 0, bytesRead);
+
+                downloaded += bytesRead;
+
+                if (downloaded >= speed) {
+                    long elapsed = System.currentTimeMillis() - start;
+                    System.out.println("Read "+ this.speed +  " bytes : " + (double)(System.currentTimeMillis() - start) + " millisecond.  downloaded: " + downloaded + " bytes. elapsed: " + elapsed  );
+                    if (elapsed < 1000) {
+                        Thread.sleep(1000 - elapsed);
+                    }
+
+                    downloaded = 0;
+                    start = System.currentTimeMillis();
+                }
+
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        byte[] dataBuffer = new byte[speed];
+
 
 
 
@@ -26,10 +81,4 @@ public class Wget implements Runnable {
         wget.start();
         wget.join();
     }
-
-
-
-
-
-
 }
