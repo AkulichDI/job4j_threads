d76d3c1b2869c466ed3844eacc2e8586b1352cc3
Gerik228 <dimaakulichd6618@gmail.com>
2026-05-14 14:52:05 +1000

    2. Модель памяти Java [#267917 #264134]

---
+++ src/main/java/concurrent/Flag.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+public class Flag {
+
+    private static volatile boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    while (flag) {
+                        System.out.println(Thread.currentThread().getName()+ " " + flag);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            System.out.println(Thread.getAllStackTraces());
+                            Thread.interrupted();
+                        }
+                    }
+                }
+        );
+        thread.start();
+        Thread.sleep(1000);
+        flag = false;
+        thread.join();
+    }
+
+
+}
