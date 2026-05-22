16fb939e3e00b534a5602b4559cd39e48b3859e9
Gerik228 <dimaakulichd6618@gmail.com>
2026-05-22 15:33:35 +1000

    0. Управление нитью через wait. [#6858 #265443]

---
+++ src/main/java/threadsLs/concurrent/CountBarrier.java
@@ -0,0 +1,47 @@
+package threadsLs.concurrent;
+
+/**
+ * Переменная total содержит количество вызовов метода count().
+ *
+ * Метод count изменяет состояние программы. Это значит, что внутри метода count нужно вызывать метод notifyAll.
+ *
+ * Нити, которые выполняют метод await, могут начать работу если поле count >= total. Если оно не равно, то нужно перевести нить в состояние wait.
+ *
+ * Здесь нужно использовать цикл while для проверки состояния, а не оператор if.
+ * */
+
+
+
+
+public class CountBarrier {
+
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {}
+
+            }
+        }
+    }
+
+
+}
